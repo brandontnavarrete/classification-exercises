12919019d97932524c1ef18e7c6eec5b55d7f87e
brandon <brandon.t.navarrete@gmail.com>
2022-12-08 09:55:14 -0600

    revisions added

--- prepare.py
+++ prepare.py
@@ -2,12 +2,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 
 
-def split_data(df):
+
+def split_data(df,col):
     
 
-    train_validate, test = train_test_split(df, test_size=.2)
-    train, validate = train_test_split(train_validate,
-                                       test_size=.3
+    train_validate, test = train_test_split(df, test_size=.2,stratify=df[col])
+    
+    train, validate = train_test_split(train_validate,test_size=.3,stratify=train_validate[col])
 
-                                       )
+
+
+                                       
     return train, validate, test
